ec9392f8e2d8d4249aa1d7d4492e5a140927dcf4
wanggenshen <genshen.wang@geely.com>
2020-04-22 23:55:11 +0800

    feat(Ch4-设计模式-迭代器模式代码示例):
    抽象类, 添加modCount

--- Ch4_CodeDesignStyle/src/main/java/com/wgs/codedesign/迭代器模式/迭代器模式实现_List迭代器/AbstractList.java
+++ Ch4_CodeDesignStyle/src/main/java/com/wgs/codedesign/迭代器模式/迭代器模式实现_List迭代器/AbstractList.java
@@ -3,7 +3,7 @@ package com.wgs.codedesign.迭代器模式.迭代器模式实现_List迭代器;
 /**
  * @author: wanggenshen
  * @date: 2020/4/21 22:43.
- * @description: XXX
+ * @description: 抽象类, 添加modCount
  */
 public abstract class AbstractList<E> implements MyList<E> {
 
