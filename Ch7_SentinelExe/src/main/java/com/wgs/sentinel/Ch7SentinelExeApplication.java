a1aa6c8cc18a1669d2d4f3a299f1e0d4ad631866
wanggenshen <genshen.wang@geely.com>
2020-06-24 11:53:10 +0800

    update

---
+++ Ch7_SentinelExe/src/main/java/com/wgs/sentinel/Ch7SentinelExeApplication.java
@@ -0,0 +1,13 @@
+package com.wgs.sentinel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch7SentinelExeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ch7SentinelExeApplication.class, args);
+	}
+
+}
---
+++ Ch7_SentinelExe/src/test/java/com/wgs/sentinel/Ch7SentinelExeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wgs.sentinel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch7SentinelExeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
